a7d3f69cf528b81027ea2f06dd1208caea279380
Vincent <10055205+gel5555@users.noreply.github.com>
2020-06-19 15:02:52 -0400

    Brought layout in line with pep8

--- fitbit_api.py
+++ fitbit_api.py
@@ -1,32 +1,39 @@
 """ Python wrapper class for the Fitbit API. 
-    Inspired by orcasgit/python-fitbit, written for practice as I learn oauth2. 
+	Inspired by orcasgit/python-fitbit, written for practice as I learn oauth2.
 """  
 
 import json  
-from ratelimit import limits, sleep_and_retry  
 import requests 
 import time 
+from ratelimit import limits, sleep_and_retry  
 
 class Fitbit:     
-	"""A simple class for calling the Fitbit WEB API, 
-	   see https://dev.fitbit.com/build/reference/web-api/
+	"""
+	A simple class for calling the Fitbit WEB API, 
+	see https://dev.fitbit.com/build/reference/web-api/
 
-	   Handles the oauth2 token refresh process automatically.
-	   Rate limited at 145 calls/hour, sleeps and retries when limit reached.
-	   
-	   Arguments:
-	   client_id: your Fitbit app client id. 
-	   client_secret: your Fitbit app client secret.
-	   access_token: an oauth2 access token.
-	   refresh_token: the corresponding refresh token.
-	   expires_at: the unix time at which access_token expires (e.g. 1591749405.1234567). 
-	   token_update_method: a method accepting a token dict and storing it as a static file,
-	   			used to refresh tokens as they expire. 
+    Handles the oauth2 token refresh process automatically. 
+    Rate limited at 145 calls/hour, sleeps and retries when limit reached.
+   
+    Attributes:  
+    client_id: your Fitbit app client id. 
 
-	"""  
+    client_secret: your Fitbit app client secret.
 
-	def __init__(self, client_id=None, client_secret=None, access_token=None, 
-				 refresh_token=None, expires_at=None, token_update_method=None):      
+    access_token: an oauth2 access token.
+
+    refresh_token: the corresponding refresh token.
+
+    expires_at: the unix time at which access_token expires 
+				(e.g. 1591749405.1234567). 
+
+    token_update_method: a method accepting a token dict and storing it as a 
+						 static file, used to refresh tokens as they expire. 
+	"""   
+
+	def __init__(self, client_id=None, client_secret=None, 
+			     access_token=None, refresh_token=None, 
+				 expires_at=None, token_update_method=None):       
 
 		self.client_id = client_id  
 		self.client_secret = client_secret 
@@ -36,18 +43,22 @@ class Fitbit:
 		self.token_update_method = token_update_method
 
 	def refresh_tokens(self): 
-		""" Called when access_token has expired. Use a valid refresh_token to 
-		    refresh the access_token, refresh_token and expires_at attributes. 
-		    Calls the token_update_method to store the updated token information. 
 		""" 
+		Called when access_token has expired. Use a valid refresh_token to 
+		refresh the access_token, refresh_token and expires_at attributes. 
+		Calls the token_update_method to store the updated token information. 
+		"""  
 
 		# Get new token dict from server 
-		response = requests.post('https://api.fitbit.com/oauth2/token', 
-					data={
-						"client_id": self.client_id, 
-						"grant_type": "refresh_token",
-						"refresh_token": self.refresh_token  
-					}, auth=(self.client_id, self.client_secret))  
+		response = requests.post(
+				url='https://api.fitbit.com/oauth2/token', 
+				data={
+					"client_id": self.client_id, 
+					"grant_type": "refresh_token",
+					"refresh_token": self.refresh_token  
+					}, 
+				auth=(self.client_id, self.client_secret)
+				)    
 
 		tokens = response.json()  
 
@@ -64,16 +75,8 @@ class Fitbit:
 		if self.token_update_method: 
 			self.token_update_method(tokens)  
 
-	def check_token_expiry(func): 
-		""" Decorator, returns a wrapper that first refreshes tokens if access_token has expired. """ 
-		def wrapper(self, *args, **kwargs):   
-			if (not self.expires_at) or (time.time() >= float(self.expires_at)): 
-				self.refresh_tokens() 
-			return func(self, *args, **kwargs)   
-		return wrapper 
-	
 	@sleep_and_retry 
-	@limits(calls=145, period=3600) 
+	@limits(calls=125, period=3600) 
 	def make_request(self, *args, **kwargs): 
 		""" Wrapper for the request method. """ 
 		response = requests.request(*args, **kwargs)
@@ -82,30 +85,16 @@ class Fitbit:
 			raise Exception('API response: {}'.format(response.status_code))
 		return response 
 
-	@check_token_expiry 
-	def get_resource(self, resource_url):  
-		""" Accepts string url as one of these resource endpoints:  
-
-		    Activities: 
-		    https://api.fitbit.com/1/user/-/activities/date/[date].json  
-		    https://api.fitbit.com/1/user/-/[resource-path]/date/[date]/[period].json 
-		    https://api.fitbit.com/1/user/-/[resource-path]/date/[base-date]/[end-date].json
-		    https://api.fitbit.com/1/user/-/[resource-path]/date/[date]/[date]/[detail-level].json 
-		    https://api.fitbit.com/1/user/-/[resource-path]/date/[date]/1d/[detail-level].json
-		    https://api.fitbit.com/1/user/-/[resource-path]/date/[date]/[date]/[detail-level]/time/[start-time]/[end-time].json
-		    https://api.fitbit.com/1/user/-/[resource-path]/date/[date]/1d/[detail-level]/time/[start-time]/[end-time].json
-
-		    Heart rate: 
-		    https://api.fitbit.com/1/user/-/activities/heart/date/[date]/[period].json 
-		    https://api.fitbit.com/1/user/-/activities/heart/date/[base-date]/[end-date].json 
-
-		    Sleep: 
-		    https://api.fitbit.com/1.2/user/-/sleep/date/[date].json 
-		    https://api.fitbit.com/1.2/user/-/sleep/date/[startDate]/[endDate].json 
-
-		    See https://dev.fitbit.com/build/reference/web-api/ for details. 
+	def get_resource(self, resource_url):   
 		""" 
+		Check and refresh token if invalid, then make GET request.
+		See https://dev.fitbit.com/build/reference/web-api/ for endpoints. 
+		""" 
+		# Check if tokens are expired 
+		if (not self.expires_at) or (time.time() >= float(self.expires_at)): 
+			self.refresh_tokens() 
+
+		# Make request, now with valid tokens 
 		headers = {'Authorization': 'Bearer {}'.format(self.access_token)} 
 		return self.make_request('GET', url=resource_url, headers=headers)   
-
 
